8bb8f6fbf77471568888d1cb9ad855523b25f488
FractalXX <fade@hotmail.hu>
2017-09-29 08:49:17 +0200

    Node beállítása Kód ES6 standard-hez igazítása

--- web/key.js
+++ web/key.js
@@ -1,18 +1,23 @@
 var keyGfx = new Image();
 keyGfx.src = "images/key.jpg";
 
-function Key (char, x, y) {
-    this.character = char;
-    this.image = new Image();
+class key {
     
-    this.x = x;
-    this.y = y;
+    constructor(char, x, y) {
+        this.character = char;
+        this.image = new Image();
+
+        this.x = x;
+        this.y = y;
+    }
+
+    draw() {
+        mainCtx.drawImage(keyGfx, this.x, this.y);
+    }
+
+    update() {
+
+    }   
 }
 
-Key.prototype.draw = function() {
-    mainCtx.drawImage(keyGfx, this.x, this.y);
-}
 
-Key.prototype.update = function() {
-    
-}
--- web/main.js
+++ web/main.js
@@ -10,25 +10,25 @@ var mainCtx = mainCanvas.getContext("2d");
 
 var keys = [];
 
-window.onload = function() {
+window.onload = function () {
     mainCanvas.width = cWidth;
     mainCanvas.height = cHeight;
     
     setInterval(update, 1000/60);
     setInterval(draw, 1000/60);
     
-    var key = new Key('A', 200, 200);
-    keys.push(key);
+    var Key = new key('A', 200, 200);
+    keys.push(Key);
 }
 
 function draw() {
-    for(var i = 0; i < keys.length; i++) {
+    /*for(var i = 0; i < keys.length; i++) {
         keys[i].draw();
-    }
+    }*/
 }
 
 function update() {
-    for(var i = 0; i < keys.length; i++) {
+    /*for(var i = 0; i < keys.length; i++) {
         keys[i].update();
-    }
+    }*/
 }
