ce34a1cf1a526501747825f1530438c48cfa8831
bbalazs79 <b.balazs79@gmail.com>
2017-10-01 17:52:21 +0200

    key osztály visible mező és kidolgozás

--- web/key.js
+++ web/key.js
@@ -13,12 +13,16 @@ class key {
         this.speedY = 0;
         this.width = keyGfx.width;
         this.heigth = keyGfx.height;
+        this.visible = false;
     }
 
     draw() {
        // mainCtx.clearRect(this.x,this.y,this.width,this.heigth);
-        mainCtx.drawImage(keyGfx,0,0,keyGfx.width,keyGfx.height, this.x, this.y,this.width,this.heigth);
-        mainCtx.fillText(this.character, this.x+this.width/2 - 24, this.y+this.heigth/2 + 24);
+        if(this.visible)
+        {
+            mainCtx.drawImage(keyGfx,0,0,keyGfx.width,keyGfx.height, this.x, this.y,this.width,this.heigth);
+            mainCtx.fillText(this.character, this.x+this.width/2 - 24, this.y+this.heigth/2 + 24); 
+        }
     }
 
     update() 
